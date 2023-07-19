4ccf189f9b77c80bb2d1bc59563ee0ff3f021c36
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-07-18 21:37:47 -0500

    fix a typo

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -5,9 +5,9 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   teamA.forEach((key, value) {
     if (key == 'Free throws') {
       teamAPoints += value;
-    } else if (key == '2 pointer') {
+    } else if (key == '2 pointers') {
       teamAPoints += value * 2;
-    } else if (key == '3 pointer') {
+    } else if (key == '3 pointers') {
       teamAPoints += value * 3;
     }
   });
@@ -15,13 +15,15 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   teamB.forEach((key, value) {
     if (key == 'Free throws') {
       teamBPoints += value;
-    } else if (key == '2 pointer') {
+    } else if (key == '2 pointers') {
       teamBPoints += value * 2;
-    } else if (key == '3 pointer') {
+    } else if (key == '3 pointers') {
       teamBPoints += value * 3;
     }
   });
-
+  // print(teamAPoints);
+  // print(teamBPoints);
+  // print(teamAPoints > teamBPoints ? 'Team A wins' : 'Team B wins');
   if (teamAPoints > teamBPoints) {
     return 1;
   } else if (teamAPoints < teamBPoints) {
