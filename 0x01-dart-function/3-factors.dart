e3f5f998dd1931c4bcfb533b6839d646e33e019c
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-07-18 20:15:32 -0500

    task 3 - Factorial

--- 0x01-dart-function/3-factors.dart
+++ 0x01-dart-function/3-factors.dart
@@ -1,11 +1,3 @@
-/*
-Write recursive function to compute the factors of a positive integer fact()
-
-    Function prototype : int fact(int f)
-        If f == 1 the function return 1
-        If f <= 0 return 0
-*/
-
 int fact(int f) {
   if (f == 1) {
     return 1;
