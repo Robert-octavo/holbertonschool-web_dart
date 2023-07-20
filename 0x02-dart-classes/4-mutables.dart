3035cd02424ad1ac7ea86bc0840a47200d15347f
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-07-20 12:14:45 -0500

    test task 4 - Mutables

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,46 @@
+class Password {
+  // make the property password private
+  String _password = "";
+
+  // constructor
+  Password({String password = ""}) {
+    _password = password;
+  }
+
+  // getter
+  String get password => _password;
+
+  // setter
+  set password(String newPassword) {
+    _password = newPassword;
+  }
+
+  /* password is valid*/
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+
+    bool hasUpper = false;
+    bool hasLower = false;
+    bool hasNumber = false;
+
+    for (int i = 0; i < _password.length; i++) {
+      if (_password[i] == _password[i].toUpperCase()) {
+        hasUpper = true;
+      }
+      if (_password[i] == _password[i].toLowerCase()) {
+        hasLower = true;
+      }
+    }
+    // check if _password contains number using RegExp
+    if (RegExp(r'[0-9]').hasMatch(_password)) {
+      hasNumber = true;
+    }
+    return hasUpper && hasLower && hasNumber;
+  }
+
+  String toString() {
+    return "Your Password is: $_password";
+  }
+}
