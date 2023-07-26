326a07f47fb2d09e393ab1b0e62775b8cab05939
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-07-25 22:22:07 -0500

    test task 3

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -1,25 +1,6 @@
 import '3-util.dart';
 import 'dart:convert';
 
-// Create a function greetUser() that returns a string as follows Hello <username> .
-
-// Function prototype : Future<String> greetUser()
-
-// Gets the user data by calling the provided function fetchUserData()
-
-// Must use try-catch If an error occurs the function should return : error caught: <error>
-
-// Create a function loginUser()
-
-// Function prototype : Future<String> loginUser()
-
-// Should call the provided function checkCredentials() which returns a future bool.
-
-//     If checkCredentials() returns true loginUser() should print There is a user: true and call greetUser() to return it’s value
-//     If checkCredentials() returns false loginUser() should print There is a user: false and return Wrong credentials
-
-// Must use try-catch If an error occurs the function should return error caught: <error>
-
 Future<String> greetUser() async {
   try {
     String data = await fetchUserData();
@@ -37,7 +18,6 @@ Future<String> loginUser() async {
     if (isUser) {
       return await greetUser();
     } else {
-      print('There is a user: $isUser');
       return 'Wrong credentials';
     }
   } catch (e) {
